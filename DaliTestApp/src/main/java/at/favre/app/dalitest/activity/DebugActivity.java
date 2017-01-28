005546564269e98c37d19db0714627899d0fa3b6
pfavre <patrick.favre@rise-world.com>
2017-01-28 22:39:13 +0100

    adds build time updates debug view

--- DaliTestApp/src/main/java/at/favre/app/dalitest/activity/DebugActivity.java
+++ DaliTestApp/src/main/java/at/favre/app/dalitest/activity/DebugActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 
 import at.favre.app.dalitest.BuildConfig;
 import at.favre.lib.hood.extended.PopHoodActivity;
+import at.favre.lib.hood.interfaces.Config;
 import at.favre.lib.hood.interfaces.Page;
 import at.favre.lib.hood.interfaces.Pages;
 import at.favre.lib.hood.util.defaults.DefaultProperties;
@@ -13,12 +14,18 @@ public class DebugActivity extends PopHoodActivity {
     @Override
     public Pages getPageData(@NonNull Pages emptyPages) {
         Page page = emptyPages.addNewPage();
-        page.add(DefaultProperties.createSectionSourceControlAndCI(BuildConfig.GIT_REV, BuildConfig.GIT_BRANCH, BuildConfig.GIT_DATE, BuildConfig.BUILD_NUMBER, null, null));
+        page.add(DefaultProperties.createSectionSourceControlAndCI(BuildConfig.GIT_REV, BuildConfig.GIT_BRANCH, BuildConfig.GIT_DATE, BuildConfig.BUILD_NUMBER, null, BuildConfig.BUILD_DATE));
         page.add(DefaultProperties.createSectionBasicDeviceInfo());
         page.add(DefaultProperties.createDetailedDeviceInfo(this));
         page.add(DefaultProperties.createSectionAppVersionInfoFromBuildConfig(BuildConfig.class));
         page.add(DefaultProperties.createSectionAppVersionInfoFromBuildConfig(at.favre.lib.dali.BuildConfig.class).removeHeader());
 
         return emptyPages;
+    }
+
+    @NonNull
+    @Override
+    public Config getConfig() {
+        return Config.newBuilder().setShowZebra(false).setLogTag("DebugActivity").build();
     }
 }
