d77e52b886e0e9ea686d2b61b86e68b7fde02a92
pfavre <patrick.favrebulle@gmail.com>
2017-11-22 21:40:07 +0100

    Fix compiler errors

--- dali/src/main/java/at/favre/lib/dali/blur/BlurKernels.java
+++ dali/src/main/java/at/favre/lib/dali/blur/BlurKernels.java
@@ -9,7 +9,7 @@ public final class BlurKernels {
     private BlurKernels() {
     }
 
-    float[] GAUSSIAN_5x5 = new float[]{
+    public static final float[] GAUSSIAN_5x5 = new float[]{
             0.0030f, 0.0133f, 0.0219f, 0.0133f, 0.0030f,
             0.0133f, 0.0596f, 0.0983f, 0.0596f, 0.0133f,
             0.0219f, 0.0983f, 0.1621f, 0.0983f, 0.0219f,
@@ -17,7 +17,7 @@ public final class BlurKernels {
             0.0030f, 0.0133f, 0.0219f, 0.0133f, 0.0030f
     };
 
-    float[] BOX_5x5 = new float[]{
+    public static final float[] BOX_5x5 = new float[]{
             0.04f, 0.04f, 0.04f, 0.04f, 0.04f,
             0.04f, 0.04f, 0.04f, 0.04f, 0.04f,
             0.04f, 0.0425f, 0.05f, 0.0425f, 0.04f,
@@ -25,10 +25,9 @@ public final class BlurKernels {
             0.04f, 0.04f, 0.04f, 0.04f, 0.04f
     };
 
-    float[] BOX_3x3 = new float[]{
+    public static final float[] BOX_3x3 = new float[]{
             0.111111111111111111111111112f, 0.111111111111111111111111112f, 0.111111111111111111111111112f,
             0.111111111111111111111111112f, 0.13f, 0.111111111111111111111111112f,
             0.111111111111111111111111112f, 0.111111111111111111111111112f, 0.111111111111111111111111112f
     };
-
 }
